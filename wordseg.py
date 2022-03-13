fe8db83db2eeb35155abc440fc67a04e58123717
wcw <496537958@qq.com>
2022-03-13 18:06:43 +0800

    update

--- wordseg.py
+++ wordseg.py
@@ -1,6 +1,6 @@
 import jieba
-f=open("20220309.txt","r",encoding='utf8')
-w=open("20220309_result.txt","w",encoding='utf8')
+f=open("data/20220313.txt","r",encoding='utf8')
+w=open("output/20220313_result.txt","w",encoding='utf8')
 con=f.readlines()
 for line in con:
     res=jieba.cut(line)
